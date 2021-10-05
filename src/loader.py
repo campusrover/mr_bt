58846efdbb36a26252d8e4395ae4bc8ba9cd2854
Adam Ring <57344340+Aringan0323@users.noreply.github.com>
2021-10-05 11:33:33 -0400

    converter from non-pythonic string format

--- src/loader.py
+++ src/loader.py
@@ -2,6 +2,24 @@ from pathlib import Path
 import inspect
 import importlib
 
+
+def module_name(string):
+
+    pythonic_string = ""
+
+    for i in range(len(string)):
+
+        letter = string[i:i+1]
+
+        if letter.isupper():
+            letter = letter.lower()
+
+            if i>0:
+                letter = "_" + letter
+        pythonic_string += letter
+
+    return pythonic_string
+
 def import_node(nodename):
 
     node_filepath = str(list(Path("nodes").rglob(nodename + ".py"))[0])
